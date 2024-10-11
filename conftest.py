34fc68ab1090329664ab6591260f7f12615bda3f
RomanErt <roovolution@gmail.com>
2024-10-11 16:42:48 -0700

    added headless mode

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from pages.sale_page import SalePage
 from pages.customer_account_create import CustomerAccountCreate
 from pages.collections_eco_friendly import CollectionsEcoFriendly
@@ -7,7 +8,9 @@ from pages.collections_eco_friendly import CollectionsEcoFriendly
 
 @pytest.fixture()
 def driver():
-    chrome_driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    chrome_driver = webdriver.Chrome(options=options)
     return chrome_driver
 
 
