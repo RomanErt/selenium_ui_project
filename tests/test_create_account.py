4769aa70dd5235bc34271efc3a24cd037dde089b
RomanErt <roovolution@gmail.com>
2024-10-12 18:12:13 -0700

    Added pytest.ini, marked tests

--- tests/test_create_account.py
+++ tests/test_create_account.py
@@ -1,8 +1,11 @@
+import pytest
 from test_data import (weak_password, medium_password, strong_password, very_strong_password, weak_password_message,
                        medium_password_message, strong_password_message, very_strong_password_message, password,
                        confirm_password, confirm_password_dont_match_error)
 
 
+@pytest.mark.regression
+@pytest.mark.smoke
 def test_required_field_error_messages_displayed(customer_account_create):
     customer_account_create.open_page()
     customer_account_create.click_create_an_account_button()
@@ -14,6 +17,8 @@ def test_required_field_error_messages_displayed(customer_account_create):
     customer_account_create.verify_confirm_password_required_error_is_displayed()
 
 
+@pytest.mark.regression
+@pytest.mark.smoke
 def test_password_strength_meter_updates_correctly(customer_account_create):
     customer_account_create.open_page()
 
@@ -33,6 +38,7 @@ def test_password_strength_meter_updates_correctly(customer_account_create):
     customer_account_create.verify_password_strength_meter_text(very_strong_password_message)
 
 
+@pytest.mark.extended
 def test_confirm_password_error_correct_if_passwords_do_not_match(customer_account_create):
     customer_account_create.open_page()
     customer_account_create.input_password_and_confirm_password(password, confirm_password)
--- tests/test_eco_friendly_collection.py
+++ tests/test_eco_friendly_collection.py
@@ -1,14 +1,20 @@
+import pytest
+
+
+@pytest.mark.regression
 def test_item_added_to_comparison_success(collections_eco_friendly):
     collections_eco_friendly.open_page()
     collections_eco_friendly.add_item_to_compare()
     collections_eco_friendly.verify_item_successfully_added_to_compare()
 
 
+@pytest.mark.regression
 def test_product_images_loaded(collections_eco_friendly):
     collections_eco_friendly.open_page()
     collections_eco_friendly.verify_product_images_load()
 
 
+@pytest.mark.extended
 def test_erin_recommends_shopping_options(collections_eco_friendly):
     collections_eco_friendly.open_page()
     collections_eco_friendly.click_erin_recommends_menu_option()
